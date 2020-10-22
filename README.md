5830ca1c02d1e5545ab2ad5744121491adf4a339
Rob Hewitt <drrobhewitt@gmail.com>
2020-10-21 19:10:54 -0700

    The stuff that I did

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # java-week-1
 Gateway cohort week 1 of Java class
+
+## Level 2
+
+### Level 3
+Notes go here.
---
+++ src/week1/App.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class App {
+
+	public static void main(String[] commandLineParameters) {
+
+		// Hi there, person
+		// You act like a 12-year-old
+		// Your hair is orange
+		
+		String person = " person";
+		String hairColor = "orange";
+				
+		System.out.println("Hi there, " + person);
+		System.out.println("You act line a " + (14 - 2) + "-year-old!");
+		System.out.println("Your hair is " + hairColor);
+	}
+
+}
